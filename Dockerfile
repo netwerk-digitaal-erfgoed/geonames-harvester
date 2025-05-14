fcdb3bf16614c5e4b0962f0ce14ef75fc501f513
David de Boer <david@ddeboer.nl>
2025-05-14 11:06:54 +0200

    feat: Process complete GeoNames (#19)
    * Use SPARQL Anything for SPARQL-based mappings.
    * Process admin codes first, preloading the output in the main mapping
      process.
    * Download all countries and simplify download script.
    * Cut data into chunks to prevent OOMs.
    * Add a GitHub Action.

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,10 @@
-FROM eclipse-temurin:17
-LABEL org.opencontainers.image.source = "https://github.com/netwerk-digitaal-erfgoed/geonames-harvester"
+FROM eclipse-temurin:21-jre
+LABEL org.opencontainers.image.source="https://github.com/netwerk-digitaal-erfgoed/geonames-harvester"
+ENV SPARQL_ANYTHING_VERSION=v1.0-DEV.15
+ENV SPARQL_ANYTHING_JAR="sparql-anything-$SPARQL_ANYTHING_VERSION.jar"
 WORKDIR /app
 RUN mkdir bin
-RUN curl -L https://github.com/RMLio/rmlmapper-java/releases/download/v5.0.0/rmlmapper-5.0.0-r362-all.jar -o bin/rmlmapper.jar
+
 RUN apt-get update && apt-get install zip -y && rm -rf /var/lib/apt/lists/*
+RUN curl -L https://github.com/SPARQL-Anything/sparql.anything/releases/download/$SPARQL_ANYTHING_VERSION/$SPARQL_ANYTHING_JAR -o bin/$SPARQL_ANYTHING_JAR
 COPY . .
--- map.sh
+++ map.sh
@@ -1,12 +1,23 @@
-#! /bin/bash
+#!/bin/sh
 DATA_DIR="$PWD/data"
 BIN_DIR="$PWD/bin"
 CONFIG_DIR="$PWD/config"
-OUTPUT_DIR="$DATA_DIR"
-cd $DATA_DIR
-echo "Running the RMLmapper to convert geonames data to RDF, be patient..."
-java -jar $BIN_DIR/rmlmapper.jar -m $CONFIG_DIR/geonames.rml -o $OUTPUT_DIR/geonames.nt
-cd ..
-# This RML mapping creates triples with empty object values.
-# The following sed command remove every line with a "". pattern.
-sed -i '/\"\"./d' $OUTPUT_DIR/geonames.nt
+: "${SPARQL_ANYTHING_VERSION:=v1.0-DEV.15}"
+: "${SPARQL_ANYTHING_JAR:=sparql-anything-$SPARQL_ANYTHING_VERSION.jar}"
+
+# Download SPARQL Anything CLI.
+if [ ! -f "$BIN_DIR/$SPARQL_ANYTHING_JAR" ]; then
+    curl -L "https://github.com/SPARQL-Anything/sparql.anything/releases/download/$SPARQL_ANYTHING_VERSION/$SPARQL_ANYTHING_JAR" -o $BIN_DIR/$SPARQL_ANYTHING_JAR
+fi
+
+# Map admin codes.
+java -jar $BIN_DIR/$SPARQL_ANYTHING_JAR -q $CONFIG_DIR/admin-codes.rq > $DATA_DIR/admin-codes.ttl
+
+# Iterate over chunks and run them through SPARQL Anything individually to prevent OOMs.
+trap "exit 1" INT
+for f in $DATA_DIR/geonames_*.csv; do
+    echo "Processing $f"
+    java -jar $BIN_DIR/$SPARQL_ANYTHING_JAR --query "$(sed "s|{SOURCE}|$f|" $CONFIG_DIR/places.rq)" --load $DATA_DIR/admin-codes.ttl --output $f.ttl
+done
+
+cat $DATA_DIR/*.csv.ttl > $DATA_DIR/geonames.ttl
